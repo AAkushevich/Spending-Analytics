e3a20948fa0763a8ae98fdc05d6535b43aeeb247
WEncelad <new.box41@gmail.com>
2020-02-20 11:16:20 +0300

    auth page

---
+++ mobile/spending_analytics/lib/ui/AuthPage/AuthScreen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:spending_analytics/ui/AuthPage/SignUpPage.dart';
+class AuthPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AuthPageState();
+  }
+
+}
+
+class AuthPageState extends State<AuthPage> {
+
+  Widget _buildPageContent(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: ListView(
+        children: <Widget>[
+          SizedBox(height: 50.0),
+          _buildLoginForm(),
+
+        ],
+      ),
+    );
+  }
+
+  Container _buildLoginForm() {
+    return Container(
+      padding: EdgeInsets.all(30.0),
+      decoration: new BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 200.0, // has the effect of softening the shadow
+            spreadRadius: -80.0, // has the effect of extending the shadow
+            offset: Offset(
+              0.0, // horizontal, move right 10
+              0.0, // vertical, move down 10
+            ),
+          )
+        ],
+//        borderRadius: new BorderRadius.all(...),
+//        gradient: new LinearGradient(...),
+      ),
+      child: Stack(
+        children: <Widget>[
+            Container(
+              height: 400,
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                color: Colors.white,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 90.0),
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: TextField(
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                            hintText: "Email address",
+                            hintStyle: TextStyle(color: Colors.black),
+                            border: InputBorder.none,
+                            icon: Icon(Icons.email, color: Colors.black,)
+                        ),
+                      )
+                  ),
+                  Container(child: Divider(color: Colors.black,), padding: EdgeInsets.only(left: 20.0,right: 20.0, bottom: 10.0),),
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: TextField(
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.black),
+                            border: InputBorder.none,
+                            icon: Icon(Icons.lock, color: Colors.black,)
+                        ),
+                      )
+                  ),
+                  Container(child: Divider(color: Colors.black), padding: EdgeInsets.only(left: 20.0,right: 20.0, bottom: 10.0)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(padding: EdgeInsets.only(right: 10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              FlatButton(
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (BuildContext context) => SignUpOnePage()
+                                  ));
+                                },
+                                child: Text("Sign Up", style: TextStyle(color: Colors.black, fontSize: 14.0)),
+                              )
+                            ],
+                          )
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                ],
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 35.0,
+                  backgroundColor: Colors.black,
+                  child: Icon(Icons.person, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 420,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton(
+                onPressed: (){},
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+                child: Text("Login", style: TextStyle(color: Colors.white)),
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: _buildPageContent(context),
+    );
+  }
+
+}
